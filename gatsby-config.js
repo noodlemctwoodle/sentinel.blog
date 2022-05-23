958d262d7abec1e3380b91082b738d9cadb517ef
noodlemctwoodle <15075388+noodlemctwoodle@users.noreply.github.com>
2022-05-23 12:01:06 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Sentinel Blog`,
+    siteTitleAlt: `sentinel.blog`,
+    siteHeadline: 'Guides and information from a defender using the Microsoft Security stack.`,
+    siteUrl: `https://sentinel.blog`,
+    siteDescription: `Guides and information from a defender using the Microsoft Security stack. Microsoft Sentinel, Microsoft Defender for Cloud, Microsoft Defender for Cloud Apps, Microsoft Defender for Endpoint, Microsoft Defender for Identity.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@noodlemctwoodle`,
   },
   plugins: [
     {
@@ -34,11 +34,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/noodlemctwoodle`,
           },
           {
             name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `https://sentinel.blog`,
           },
         ],
       },
